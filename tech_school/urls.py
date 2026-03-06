bf5ff0619d80b31f430d9fa1178d84668c4725fd
Kwanusu <kwanusujoseph@gmail.com>
2026-03-06 18:07:34 +0300

    create user views and connect urls

--- tech_school/urls.py
+++ tech_school/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from .views import UserViewSet
 
 router = DefaultRouter()
+router.register(r'auth', UserViewSet, basename='auth')
 
 urlpatterns = [
     path('', include(router.urls))
--- tech_school/views.py
+++ tech_school/views.py
@@ -1,3 +1,71 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status, permissions
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.contrib.auth import get_user_model, authenticate
+from rest_framework_simplejwt.tokens import RefreshToken
+from .serializers import UserSerializer, RegisterSerializer
 
-# Create your views here.
+User = get_user_model()
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def get_permissions(self):
+        if self.action in ['register', 'create', 'login']:
+            return [permissions.AllowAny()]
+        if self.action in ['list', 'destroy']:
+            return [permissions.IsAdminUser()]
+        return [permissions.IsAuthenticated()]
+
+    @action(detail=False, methods=['get'], permission_classes=[permissions.IsAuthenticated])
+    def me(self, request):
+        """
+        Endpoint: /api/users/me/
+        Returns a logged in user
+        """
+        serializer = self.get_serializer(request.user)
+        return Response(serializer.data)
+
+    def get_serializer_class(self):
+        if self.action == 'create':
+            return RegisterSerializer
+        return UserSerializer
+    
+    @action(detail=False, methods=['post'], permission_classes=[permissions.AllowAny])
+    def register(self, request):
+        """
+        Endpoint: POST /api/auth/register/
+        """
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            refresh = RefreshToken.for_user(user)
+            return Response({
+                "user": UserSerializer(user).data,
+                "token": str(refresh.access_token),
+            }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @action(detail=False, methods=['post'])
+    def login(self, request):
+        """
+        Endpoint: POST /api/users/login/
+        """
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(username=username, password=password)
+
+        if user:
+            refresh = RefreshToken.for_user(user)
+            return Response({
+                'token': str(refresh.access_token),
+                'user': UserSerializer(user).data
+            }, status=status.HTTP_200_OK)
+        
+        return Response({'error': 'Invalid Credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+
+
+ 
+    
+    
