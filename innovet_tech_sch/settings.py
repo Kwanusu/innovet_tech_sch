ca86834cce0d26615a6a39e2b519f473e0e7d1fa
Kwanusu <kwanusujoseph@gmail.com>
2026-03-08 14:35:20 +0300

    update email port

--- innovet_tech_sch/settings.py
+++ innovet_tech_sch/settings.py
@@ -120,7 +120,7 @@ if DEBUG:
 else:    
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_HOST = os.environ.get('EMAIL_HOST')
-    EMAIL_PORT = int(os.environ.get('EMAIL_PORT'))
+    EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
     EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True') == 'True'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')  
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
